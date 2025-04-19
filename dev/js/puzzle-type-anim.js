1e4a70443cf7362b25b8e488d255cb754c1fac9e
evangwhit <183247776+EvanWhitaker@users.noreply.github.com>
2025-04-18 20:26:55 -0400

    💄 Small edits

--- dev/js/puzzle-type-anim.js
+++ dev/js/puzzle-type-anim.js
@@ -5,7 +5,7 @@ Puzzle type animations
 - Add the class "puzzle-hover" if you want the effect to apply on hover (for nav)
 - Add the class "puzzle-auto" if you want the effect to play on load (for page titles)
 
------*/
+/-----------------*/
 
 document.addEventListener("DOMContentLoaded", () => {
     const textElements = document.querySelectorAll(".puzzle-type")
@@ -13,6 +13,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
     textElements.forEach((textElement) => {
     // Here is where to adjust the spacing of the grid, if you want the tracking to be more or less
+
+    /* --------------------------------------------------------------------------------
+    
+    Here I need to fix the spacing Factor so that it can scale proportionally based on screen size in addition to the font size scaling
+
+    -----------------------------------------------------------------------------------*/
+    
         const spacingFactor = .75;
         const spacingTranslate = 13;
 
